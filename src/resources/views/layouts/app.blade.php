3426a4eb2fd4f3d449c0abaa1d537ca3038dd71a
yurin <hoshinana.eight.nine.ten.y@gmail.com>
2026-05-26 06:01:51 +0900

    会員登録画面とログイン画面のスタイル作成

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <title>COACHTECH</title>
     <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    @yield('css')
 </head>
 
 <body>
