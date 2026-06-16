31dfa761bcd711ad2ce29d59f587b7be233cd0c9
yurin <hoshinana.eight.nine.ten.y@gmail.com>
2026-06-16 23:58:57 +0900

    メール認証についてCSSを追加　　

--- src/resources/views/auth/verify-email.blade.php
+++ src/resources/views/auth/verify-email.blade.php
@@ -1,17 +1,23 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/verify-email.css') }}">
+@endsection
+
 @section('content')
-<p>登録していただいたメールアドレスに認証メールを送付しました。</p>
-<p>メール認証を完了してください。</p>
+<div class="auth-container">
+    <p>登録していただいたメールアドレスに認証メールを送付しました。</p>
+    <p>メール認証を完了してください。</p>
 
-@if (session('status') == 'verification-link-sent')
-<p style="color: green;">新しい認証メールを再送信しました。</p>
-@endif
+    @if (session('status') == 'verification-link-sent')
+    <p style="color: green;">新しい認証メールを再送信しました。</p>
+    @endif
 
-<a href="http://localhost:8025" target="_blank" class="btn">認証はこちらから</a>
+    <a href="http://localhost:8025" target="_blank" class="btn-verify">認証はこちらから</a>
 
-<form method="POST" action="{{ route('verification.send') }}">
-    @csrf
-    <button type="submit">認証メールを再送する</button>
-</form>
+    <form method="POST" action="{{ route('verification.send') }}">
+        @csrf
+        <button type="submit" class="btn-resend">認証メールを再送する</button>
+    </form>
+</div>
 @endsection
